fe5df225970043124c027322f0e463c7f06d0ec2
ghkdwlgns612 <ghkdwlgns612@naver.com>
2020-10-26 10:22:15 +0900

    Dynamic banner HTML,CSS(ballon!!)

---
+++ main.js
@@ -0,0 +1,7 @@
+let banner = document.getElementById("banner"),
+    img = banner.getElementsByTagName("img"),
+    toggle = document.getElementById("toggle"),
+    sound_btn = document.getElementById("sound_btn");
+
+let banner_height = getComputedStyle(banner).height;
+let cast = [];
